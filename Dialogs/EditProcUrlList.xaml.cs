fa05cdc9ffb38b504e9ff039eb9f882e4d13d636
斟酌 鵬兄 <tgckpg@gmail.com>
2016-01-03 06:18:37 +0800

    UrlEdit Dialog

--- Dialogs/EditProcUrlList.xaml.cs
+++ Dialogs/EditProcUrlList.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using libtaotu.Models.Procedure;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,30 +13,80 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Content Dialog item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using libtaotu.Models.Procedure;
 
 namespace libtaotu.Dialogs
 {
     sealed partial class EditProcUrlList : ContentDialog
     {
-        private ProcUrlList procUrlList;
-
-        public EditProcUrlList()
+        private ProcUrlList EditTarget;
+        private EditProcUrlList()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         public EditProcUrlList( ProcUrlList procUrlList )
+            :this()
         {
-            this.procUrlList = procUrlList;
+            EditTarget = procUrlList;
+            UrlList.ItemsSource = EditTarget.Urls;
+            UrlInput.KeyDown += UrlInput_KeyDown;
+        }
+
+        private void UrlInput_KeyDown( object sender, KeyRoutedEventArgs e )
+        {
+            if( e.Key == Windows.System.VirtualKey.Enter )
+            {
+                TryAddUrl();
+            }
         }
 
         private void ContentDialog_PrimaryButtonClick( ContentDialog sender, ContentDialogButtonClickEventArgs args )
         {
         }
 
-        private void ContentDialog_SecondaryButtonClick( ContentDialog sender, ContentDialogButtonClickEventArgs args )
+        private void AddUrl( object sender, RoutedEventArgs e )
         {
+            TryAddUrl();
+        }
+
+        private void RemoveUrl( object sender, RoutedEventArgs e )
+        {
+            Button B = sender as Button;
+            string s = B.DataContext as string;
+            EditTarget.Urls.Remove( s );
+
+            UrlList.ItemsSource = null;
+            UrlList.ItemsSource = EditTarget.Urls;
+        }
+
+        private void TryAddUrl()
+        {
+            string url = UrlInput.Text.Trim();
+            if ( string.IsNullOrEmpty( url ) ) return;
+
+            try
+            {
+                Uri u = new Uri( url );
+                switch( u.Scheme )
+                {
+                    case "http":
+                    case "https":
+                        break;
+                    default:
+                        throw new InvalidDataException( "Url" );
+                }
+
+                EditTarget.Urls.Add( url );
+
+                UrlInput.Text = "";
+                UrlList.ItemsSource = null;
+
+                UrlList.ItemsSource = EditTarget.Urls;
+            }
+            catch( Exception )
+            {
+            }
         }
     }
 }
